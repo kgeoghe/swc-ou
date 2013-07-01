d91de469f075f4ebc377f2b45704818df58d13e3
Kevin Geoghegan <kgeoghe@gmail.com>
2013-07-01 14:29:44 -0500

    Added module with functions

--- add_numb.py
+++ add_numb.py
@@ -1,7 +1,7 @@
-def add_two(a,b):
-   return a+b
+#from adding import add_two, add_three
 
 def main():
+   print ("2+2+2 =", add_three(2,2,2))
    print ("2 + 3 = ",add_two(2,3))
 
 if __name__ == "__main__":
